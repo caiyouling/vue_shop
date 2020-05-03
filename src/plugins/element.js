cf6935b0def49d5a1c7e98ecb530d5c75942aecf
caiyoulingcom <13427857046@163.com>
2020-05-03 17:51:09 +0800

    订单管理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -47,6 +47,9 @@ import {
 
 } from 'element-ui'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -83,6 +86,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
     // 需要挂载到Vue上
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
